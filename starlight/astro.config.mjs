23addddf57323a9035e5526aa23f661e14f74219
trueberryless <trueberryless+github@gmail.com>
2023-10-18 19:01:56 +0200

    Initial commit
    version-patch

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -1,27 +1,29 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: "trueberryless.org",
+            social: {
+                github: "https://github.com/trueberryless-org",
+            },
+            logo: {
+                src: "./src/assets/logo.png",
+                replacesTitle: true,
+            },
+            sidebar: [
+                { label: "Home", link: "/" },
+                {
+                    label: "Projects",
+                    items: [
+                        { label: "Mutanuq", link: "/mutanuq" },
+                        { label: "TrainIT", link: "/trainit" },
+                    ],
+                },
+            ],
+            customCss: ["./src/style.css"],
+        }),
+    ],
 });
